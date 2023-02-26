235381a339d759a83b2fbfc27e455767f38210b9
uppper-productions <99124573+uppper-productions@users.noreply.github.com>
2023-02-26 17:23:41 +0100

    Added one forgotten JS

---
+++ Website/herosec-content.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".auto-type", {
+    strings: ["Stay safe on Discord.", "Prevent scams.", "Contribute to the safety of Discord!"],
+    typeSpeed: 50,
+    backSpeed: 25,
+   loop: true
+ })
