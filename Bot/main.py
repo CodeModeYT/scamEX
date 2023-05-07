6588c34aaa1da7d557c0354650cccf14a9a6a844
uppper-productions <99124573+uppper-productions@users.noreply.github.com>
2023-05-07 11:05:20 +0200

    added bot activity

--- Bot/main.py
+++ Bot/main.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 import os
 import discord
 from discord import app_commands
-
+import random
 
 # Startup
 load_dotenv()
@@ -11,8 +11,6 @@ intents = discord.Intents.default()
 client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
 report_channel = client.get_channel(1104404031713054730)
-
-
 
 
 @tree.command(name = "checkserver", description = "Check if a Server is registered at scamEX by ID", guild=discord.Object(id=969274610275192882)) 
@@ -32,7 +30,7 @@ async def serverreport(interaction, server_id: int, reason: str, invite: str):
     reported_reason=reason
     reported_server_invite=invite
     await interaction.channel.send(f"Thanks for your report, we will check it and get back to you! You can report again in 24h")
-    embedVar = discord.Embed(title="New server report - ID soon", description="Desc", color=0x00ff00)
+    embedVar = discord.Embed(title="Server Report - soon", description="Desc", color=0x00ff00)
     embedVar.add_field(name="User information", value=f"Username: \n User-ID:", inline=False)
     embedVar.add_field(name="Server information", value=f"Server-ID: {reported_server_id} \n Reason: {reported_reason} \n Server invie: {reported_server_invite}", inline=False)
     channel = client.get_channel(1104404031713054730)
@@ -42,5 +40,7 @@ async def serverreport(interaction, server_id: int, reason: str, invite: str):
 @client.event
 async def on_ready():
     await tree.sync(guild=discord.Object(id=969274610275192882))
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="out for scams"))
     print("Logged in")
+
 client.run(token)
