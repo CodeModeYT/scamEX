a8a7e8b189cfb6f5e3fea0ef575b6be51682b61d
uppper-productions <99124573+uppper-productions@users.noreply.github.com>
2023-05-08 14:31:50 +0200

    -

--- Bot/main.py
+++ Bot/main.py
@@ -30,17 +30,19 @@ async def serverreport(interaction, server_id: int, reason: str, invite: str):
     reported_reason=reason
     reported_server_invite=invite
     await interaction.channel.send(f"Thanks for your report, we will check it and get back to you! You can report again in 24h")
-    embedVar = discord.Embed(title="Server Report - soon", description="Desc", color=0x00ff00)
+    embedVar = discord.Embed(title="Server Report", description="ID: soon", color=0x00ff00)
     embedVar.add_field(name="User information", value=f"Username: \n User-ID:", inline=False)
     embedVar.add_field(name="Server information", value=f"Server-ID: {reported_server_id} \n Reason: {reported_reason} \n Server invie: {reported_server_invite}", inline=False)
     channel = client.get_channel(1104404031713054730)
     await channel.send(embed=embedVar)
     print("Got a new Report with report-ID = soon")
+ 
 
 @client.event
 async def on_ready():
     await tree.sync(guild=discord.Object(id=969274610275192882))
-    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="out for scams"))
-    print("Logged in")
+    guilds = len(client.guilds)
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"out for scams on {guilds} servers"))
+    print(f"Logged in on {guilds} guilds")
 
 client.run(token)
