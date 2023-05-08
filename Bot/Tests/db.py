9d39be5e6782449f1451d1931107d0a6de2ce74f
uppper-productions <99124573+uppper-productions@users.noreply.github.com>
2023-05-08 15:10:54 +0200

    added first database tests

--- Bot/Tests/db.py
+++ Bot/Tests/db.py
@@ -1,2 +1,21 @@
 import sqlite3
 
+connection = sqlite3.connect('scamEX\Bot\Tests\mydata.db')
+cursor = connection.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS scams(
+    server_id INTEGER
+    server_invite TEXT
+    server_reason TEXT
+)
+""")
+
+cursor.execute("""
+INSERT INTO scams VALUES
+(('888428503261909042', 'https://discord.gg/td4FhYgK5V', 'This is a test be aware!'))
+""")
+
+
+connection.commit()
+connection.close()
