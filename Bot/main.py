ee74f259788afcd2fd3116fb86d63435d0454a0c
uppper-productions <99124573+uppper-productions@users.noreply.github.com>
2023-05-09 21:18:15 +0200

    added timestamp

--- Bot/main.py
+++ Bot/main.py
@@ -2,7 +2,8 @@ from dotenv import load_dotenv
 import os
 import discord
 from discord import app_commands
-import random
+import datetime
+import time
 
 # Startup
 load_dotenv()
@@ -53,13 +54,26 @@ async def serverreport(interaction, user_id: int, reason: str):
     interaction_server = interaction.guild
     interaction_server_name = interaction_server.name
     interaction_server_id = interaction_server.id
-    await interaction.response.send_message(f"Thanks for your report, we will check it and get back to you! You can report again in 24h", ephemeral=True)
+    def create_discord_timestamp(target_time):
+        unix_timestamp = int(target_time.timestamp())
+        discord_timestamp = f"<t:{unix_timestamp}:R>"
+        return discord_timestamp
+
+    def create_future_timestamp():
+        current_time = datetime.datetime.now()
+        target_time = current_time + datetime.timedelta(hours=24)
+        discord_timestamp = create_discord_timestamp(target_time)
+        return discord_timestamp
+    future_timestamp = create_future_timestamp()
+    await interaction.response.send_message(f"Thanks for your report, we will check it and get back to you! You can report again {future_timestamp}", ephemeral=True)
     embedVar = discord.Embed(title="User Report", description="ID: soon", color=0x00ff00)
     embedVar.add_field(name="User information", value=f"Username: {username}\n User-ID: {user_id} \n from Server name: {interaction_server_name} \nfrom Server ID: {interaction_server_id}", inline=False)
     embedVar.add_field(name="Server information", value=f"Server-ID: {reported_user_id} \n Reason: {reported_reason}", inline=False)
     channel = client.get_channel(1104404031713054730)
     await channel.send(embed=embedVar)
     print("Got a new USER Report with report-ID = soon")
+    time.sleep(24 * 60 * 60)
+    await interaction.respo
 
 # MOD - only commmands
 @tree.command(name="addscam", description="MOD ONLY - Add a new scam to the system", guild=discord.Object(id=1057656714163404830))
